6c77da1a9207f548c4acd4ce2d35cff2835fd13a
Felipe Montenegro Ribeiro <felipe.mribeiro@al.infnet.edu.br>
2021-03-16 16:03:11 -0300

    processo de compra de BTC

--- api.js
+++ api.js
@@ -1,6 +1,53 @@
 const axios = require('axios')
+const qs = require('querystring')
+const crypto = require('crypto')
 
-const ENDPOINT_API = 'https://www.mercadobitcoin.net/api/'
+const ENDPOINT_API = 'https://www.mercadobitcoin.net/api/'  //API de consulta de dados
+const ENDPOINT_TRADE_PATH = '/tapi/v3/'
+const ENDPOINT_TRADE_API = 'https://www.mercadobitcoin.net' + ENDPOINT_TRADE_PATH    //Api de Trader
+
+class MercadoBitcoinTrade {
+
+    constructor(config){
+        this.config = {
+            KEY: config.key,   //chaves secretas de API para Trade
+            SECRET: config.secret,
+            PIN: config.pin,
+            CURRENCY: config.currency
+        }
+    }
+
+    placeBuyOrder(qty, limit_price) {
+        return this.call('place_buy_order', {    //Ordem de compra -> R$ p/ BTC
+            coin_pair: `BRL${this.config.CURRENCY}`,
+            quantily: `${qty}`.substring(0, 10),   //no máximo 10 números, incluindo depois da vírgula (BTC fracionado)
+            limit_price: `${limit_price}`
+        })
+
+    }
+        async call(method, parameters) {
+
+            const now = new Date().getTime()
+            let queryString = qs.stringify({ tapi_method: method, tapi_nonce: now})  // nonce -  número arbitrário que só pode ser usado uma vez
+                if(parameters) queryString += `&${qs.stringify(parameters)}`  //concatenar o vlaor em formato querystring
+            
+            const signature = crypto.createHmac('sha512', this.config.SECRET)
+                .update(`${ENDPOINT_TRADE_PATH}?${queryString}`)  // Criptografando o path completo da requisição com osecret usando algoritmo de Hash sha512
+                .digest('hex')
+
+            const config = {
+                headers: {
+                    'TAPI-ID': this.config.KEY,
+                    'TAPI-MAC': signature   // criptografa a assinatura
+
+                }
+            }
+
+            const response = await axios.post(ENDPOINT_TRADE_API, queryString, config)
+                if (response.data.error_message) throw new Error(response.data.error_message)
+                    return response.data.response_data
+    }
+}
 
 class MercadoBitcoin {
 
@@ -16,7 +63,7 @@ class MercadoBitcoin {
 
     async call(method) {
 
-        const config = {
+        const config = {   //pega dados na Api Bitcoin
             headers: {
                 'Accept': 'application/json',
 
@@ -24,8 +71,8 @@ class MercadoBitcoin {
         }
 
     try {
-        const response = await axios.get(ENDPOINT_API + this.config.CURRENCY + '/' + method, config)
-        return response.data
+        const response = await axios.get(ENDPOINT_API + this.config.CURRENCY + '/' + method, config)  
+        return response.data 
     }
     catch(error){
         console.error(error)
@@ -35,5 +82,6 @@ class MercadoBitcoin {
 }
 
 module.exports = {
-    MercadoBitcoin
+    MercadoBitcoin,
+    MercadoBitcoinTrade
 }
--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 require('dotenv-safe').config()
-const { MercadoBitcoin } = require('./api')
+const { MercadoBitcoin, MercadoBitcoinTrade } = require('./api')
 
 const infoApi = new MercadoBitcoin({ currency: 'BTC' })
+const tradeApi = new MercadoBitcoinTrade({
+    currency: 'BTC',
+    key: process.env.KEY,
+    secret: process.env.SECRET,
+    pin: process.env.PIN,
+})
+
 
 setInterval (async () => {
     const response = await infoApi.ticker()
     console.log(response)
-    if(response.ticker.sell > 339000) //valor colido após consulta
+        if(response.ticker.sell > 339000) //valor colido após consulta
        return console.log('Está caro! Pore favor, aguardar!')
-    else
-        return console.log('Está barato!! Pode comprar!!')
-
-}, process.env.CRAWLER_INTERVAL )
+    //else
+        //return console.log('Está barato!! Pode comprar!!')
+        tradeApi.placeBuyOrder(1, response.ticker.sell)  // compro 1 BTC, pelo menor preço de venda que está retornando (o sell)
+        
+}, process.env.CRAWLER_INTERVAL ) //processo do node . ambiente . variável com valor de tempo de execução
